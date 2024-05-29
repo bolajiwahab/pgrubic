9be0d3b35f6e64c2c8100a93fdbfefbb9502caf4
Bolaji Wahab <bolaji.wahab@n26.com>
2024-05-29 03:25:15 +0200

    WIP

--- src/pgshield/__main__.py
+++ src/pgshield/__main__.py
@@ -3,8 +3,10 @@
 import sys
 from collections import abc
 
-from pgshield import utils, rules_directories
+from pgshield import utils, rules_directories, config
 from pgshield import linter as _linter
+
+import pathlib
 
 
 def cli(argv: abc.Sequence[str] = sys.argv) -> None:
@@ -15,10 +17,22 @@ def cli(argv: abc.Sequence[str] = sys.argv) -> None:
 
     loaded_rules: list[_linter.Checker] = utils.load_rules(rules_directories)
 
+    loaded_codes: list[str] = [rule.code for rule in loaded_rules]
+
     utils.check_duplicate_rules(loaded_rules)
 
+    loaded_config = config.load_config(
+        pathlib.Path(
+            "/Users/bolajiwahab/repos/bolajiwahab/pgshield/src/pgshield/.pgshield"
+        ),
+    )
+
     for rule in loaded_rules:
-        linter.checkers.add(rule())
+        if rule.code in loaded_config.get(
+            "lint.select", loaded_codes,
+        ) and rule.code not in loaded_config.get("lint.ignore", []):
+
+            linter.checkers.add(rule())
 
     for source_path in source_paths:
         linter.run(source_path)
--- src/pgshield/test.py
+++ src/pgshield/test.py
@@ -1,3 +1,17 @@
-output = [(12.2817, 12.2817), (0, 0), (8.52, 8.52)]
+"""Load config from a file."""
+import configparser
+import os
+import pathlib
+import typing
 
-print(list(sum(output, ())))
+file_path = pathlib.Path(__file__).resolve().parent
+
+file_name: pathlib.Path = pathlib.Path(file_path) / ".pgshield"
+def read_config_file(file_name: os.PathLike[str]) -> dict[str, str]:
+    """Read config file from a file."""
+    kw: dict[str, typing.Any] = {}
+    config = configparser.ConfigParser(delimiters="=", **kw)
+
+    config.read(file_name)
+
+    return dict(config.items("pgshield"))
