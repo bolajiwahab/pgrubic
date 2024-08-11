d0f88019caf6155ebaad0e9f836d3a18779d49bf
Bolaji Wahab <bolaji.wahab@n26.com>
2024-08-11 02:13:49 +0200

    Test drop column

--- tests/test_UN001.py
+++ tests/test_UN001.py
@@ -9,11 +9,12 @@ def test_UN001() -> None:
     sql = """
     ALTER TABLE public.ecdict DROP COLUMN id;
     """
+    drop_column = UN001.DropColumn()
     config: core.Config = core.parse_config()
     linter: core.Linter = core.Linter(config=config)
-    UN001.DropColumn.code = "UN001"
-    assert UN001.DropColumn.is_auto_fixable is False
-    linter.checkers.add(UN001.DropColumn())
+    assert drop_column.is_auto_fixable is False
+    assert drop_column.code == "UN001"
+    linter.checkers.add(drop_column)
     violations = linter.run(source_path="test.sql", source_code=sql)
     assert violations == core.ViolationMetric(
         violations_total=1,
@@ -21,3 +22,4 @@ def test_UN001() -> None:
         violations_fixable_auto_total=0,
         violations_fixable_manual_total=1,
     )
+    assert next(iter(drop_column.violations)).description == "Drop column found"
