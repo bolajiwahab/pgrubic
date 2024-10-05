0441263e0af8a34cb6e51429e9903f54fa7a9bd8
Bolaji Wahab <bolaji.wahab@n26.com>
2024-10-06 00:02:08 +0200

    Ensuring docs are up to date

--- tools/ensure_up_to_date_docs.sh
+++ tools/ensure_up_to_date_docs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# checks/ensure_docs_built.sh
-if git diff --cached --quiet -- docs/; then
-#    git diff --name-status HEAD~1 HEAD -- docs/ | grep -E '^(A|D)'
+# tools/ensure_up_to_date_docs.sh
+
+if git diff --quiet -- docs/ && [ -z "$(git ls-files --others --exclude-standard docs/)" ]; then
     echo "Documentation are up to date."
     exit 0
 else
-    echo "Documentations are not up to date. Ensure docs are built."
+    echo "Documentations are not up to date. Ensure docs are rebuilt."
     echo "Run 'tox -e docbuild' and commit the changes."
     exit 1
 fi
