ef5d5070c325ad59e1695f4b3b54e0aef99bda0a
Bolaji Wahab <bolaji.wahab@n26.com>
2025-01-03 04:13:43 +0100

    tests: improve coverage

--- src/pgrubic/formatters/dml/cte.py
+++ src/pgrubic/formatters/dml/cte.py
@@ -6,10 +6,12 @@ from pglast import ast, stream, printers
 @printers.node_printer(ast.WithClause, override=True)
 def with_clause(node: ast.WithClause, output: stream.RawStream) -> None:
     """Printer for WithClause."""
-    relindent = -2
+    relative_indent = -2
+
     if node.recursive:
-        relindent -= output.write("RECURSIVE ")
-    output.print_list(node.ctes, relative_indent=relindent, standalone_items=False)
+        relative_indent -= output.write("RECURSIVE ")
+
+    output.print_list(node.ctes, relative_indent=relative_indent, standalone_items=False)
 
 
 @printers.node_printer(ast.CommonTableExpr, override=True)
@@ -21,25 +23,26 @@ def common_table_expr(node: ast.CommonTableExpr, output: stream.RawStream) -> No
         with output.expression(need_parens=True):
             output.print_name(node.aliascolnames, ",")
         output.indent(amount=-1, relative=False)
-        output.newline()
 
     output.swrite("AS")
     printers.dml.cte_materialize_printer(node.ctematerialized, node, output)
     output.space()
+
     with output.expression(need_parens=False):
         output.write("(")
         output.newline()
         output.indent(4)
         output.print_node(node.ctequery)
         output.indent(amount=-4, relative=False)
-        output.write("\n)")
+        output.newline()
+        output.write(")")
+
     if node.search_clause:
-        output.newline()
-        output.newline()
+        output.space()
         output.print_node(node.search_clause)
+
     if node.cycle_clause:
-        output.newline()
-        output.newline()
+        output.space()
         output.print_node(node.cycle_clause)
     if node.aliascolnames:
         output.dedent()
--- src/pgrubic/formatters/dml/select.py
+++ src/pgrubic/formatters/dml/select.py
@@ -6,19 +6,17 @@ from pglast import ast, enums, stream, printers
 @printers.node_printer(ast.SubLink, override=True)
 def sub_link(node: ast.SubLink, output: stream.RawStream) -> None:
     """Printer for SubLink."""
-    slt = enums.SubLinkType
-
-    if node.subLinkType == slt.EXISTS_SUBLINK:
+    if node.subLinkType == enums.SubLinkType.EXISTS_SUBLINK:
         output.write("EXISTS")
         output.space()
-    elif node.subLinkType == slt.ALL_SUBLINK:
+    elif node.subLinkType == enums.SubLinkType.ALL_SUBLINK:
         output.print_node(node.testexpr)
         output.space()
         output.write(printers.dml.get_string_value(node.operName))
         output.space()
         output.write("ALL")
         output.space()
-    elif node.subLinkType == slt.ANY_SUBLINK:
+    elif node.subLinkType == enums.SubLinkType.ANY_SUBLINK:
         output.print_node(node.testexpr)
         if node.operName:
             output.space()
@@ -30,13 +28,13 @@ def sub_link(node: ast.SubLink, output: stream.RawStream) -> None:
             output.space()
             output.write("IN")
             output.space()
-    elif node.subLinkType == slt.EXPR_SUBLINK:
+    elif node.subLinkType == enums.SubLinkType.EXPR_SUBLINK:
         pass
-    elif node.subLinkType == slt.ARRAY_SUBLINK:
+    elif node.subLinkType == enums.SubLinkType.ARRAY_SUBLINK:
         output.write("ARRAY")
     elif node.subLinkType in (
-        slt.MULTIEXPR_SUBLINK,
-        slt.ROWCOMPARE_SUBLINK,
+        enums.SubLinkType.MULTIEXPR_SUBLINK,
+        enums.SubLinkType.ROWCOMPARE_SUBLINK,
     ):
         msg = f"SubLink of type {node.subLinkType} not supported yet"
         raise NotImplementedError(msg)
