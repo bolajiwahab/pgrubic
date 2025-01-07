6d330a66368d55971f96c5aa1412e17d2c13345f
Bolaji Wahab <bolaji.wahab@n26.com>
2025-01-07 01:04:33 +0100

    chore: improve join, select, delete formatters

--- src/pgrubic/formatters/dml/join.py
+++ src/pgrubic/formatters/dml/join.py
@@ -11,7 +11,11 @@ def join_expr(node: ast.JoinExpr, output: stream.RawStream) -> None:
         if node.jointype in (enums.JoinType.JOIN_RIGHT, enums.JoinType.JOIN_INNER)
         else -5
     )
-    with output.push_indent(amount=indent):
+
+    if node.isNatural:
+        indent -= 1
+
+    with output.push_indent(indent):
         with output.expression(bool(node.alias)):
             output.print_node(node.larg)
             output.newline()
@@ -20,21 +24,20 @@ def join_expr(node: ast.JoinExpr, output: stream.RawStream) -> None:
                 output.write("NATURAL")
                 output.space()
 
-            jt = enums.JoinType
-            if node.jointype == jt.JOIN_INNER:
+            if node.jointype == enums.JoinType.JOIN_INNER:
                 if not node.usingClause and not node.quals and not node.isNatural:
                     output.write("CROSS")
                     output.space()
                 else:
                     output.write("INNER")
                     output.space()
-            elif node.jointype == jt.JOIN_LEFT:
+            elif node.jointype == enums.JoinType.JOIN_LEFT:
                 output.write("LEFT")
                 output.space()
-            elif node.jointype == jt.JOIN_FULL:
+            elif node.jointype == enums.JoinType.JOIN_FULL:
                 output.write("FULL")
                 output.space()
-            elif node.jointype == jt.JOIN_RIGHT:
+            elif node.jointype == enums.JoinType.JOIN_RIGHT:
                 output.write("RIGHT")
                 output.space()
 
@@ -43,12 +46,9 @@ def join_expr(node: ast.JoinExpr, output: stream.RawStream) -> None:
 
             if isinstance(node.rarg, ast.JoinExpr):
                 output.indent(3, relative=False)
-                # need this for:
-                # tests/test_printers_roundtrip.py::test_pg_regress_corpus[join.sql] -
-                # AssertionError: Statement “select * from   int8_tbl x cross join
-                # (int4_tbl x cross join lateral (select x.f1) ss)”
-                # from libpg_query/test/sql/postgres_regress/join.sql at line 1998
+                # TODO: This needs to be fixed to handle nested JOIN expressions properly  # noqa: TD002, FIX002, TD003, E501
                 with output.expression(not bool(node.rarg.alias)):
+                    output.indent(4)
                     output.print_node(node.rarg)
                 output.newline()
             else:
@@ -66,18 +66,19 @@ def join_expr(node: ast.JoinExpr, output: stream.RawStream) -> None:
                     output.print_node(node.join_using_alias)
             elif node.quals:
                 output.newline()
-                (
+                if node.jointype in (
+                    enums.JoinType.JOIN_RIGHT,
+                    enums.JoinType.JOIN_INNER,
+                ):
                     output.space(3)
-                    if node.jointype
-                    in (enums.JoinType.JOIN_RIGHT, enums.JoinType.JOIN_INNER)
-                    else output.space(2)
-                )
+                else:
+                    output.space(2)
                 output.swrite("ON")
                 output.space()
                 output.print_node(node.quals)
 
         if node.alias:
-            output.newline()
+            output.space()
             output.writes("AS")
             output.space()
             output.print_name(node.alias)
--- src/pgrubic/formatters/dml/select.py
+++ src/pgrubic/formatters/dml/select.py
@@ -18,6 +18,7 @@ def sub_link(node: ast.SubLink, output: stream.RawStream) -> None:
         output.space()
     elif node.subLinkType == enums.SubLinkType.ANY_SUBLINK:
         output.print_node(node.testexpr)
+
         if node.operName:
             output.space()
             output.write(printers.dml.get_string_value(node.operName))
@@ -28,11 +29,11 @@ def sub_link(node: ast.SubLink, output: stream.RawStream) -> None:
             output.space()
             output.write("IN")
             output.space()
-    elif node.subLinkType == enums.SubLinkType.EXPR_SUBLINK:
-        pass
+
     elif node.subLinkType == enums.SubLinkType.ARRAY_SUBLINK:
         output.write("ARRAY")
-    elif node.subLinkType in (
+        output.space()
+    elif node.subLinkType in (  # pragma: no cover
         enums.SubLinkType.MULTIEXPR_SUBLINK,
         enums.SubLinkType.ROWCOMPARE_SUBLINK,
     ):
@@ -41,8 +42,22 @@ def sub_link(node: ast.SubLink, output: stream.RawStream) -> None:
 
     with output.expression(need_parens=False):
         bool_in_ancestors = ast.BoolExpr in node.ancestors
-        indent = 7 if bool_in_ancestors else 10
-        dedent = -3 if bool_in_ancestors else -3
+        nearest_node: ast.Node = abs(node.ancestors).node
+
+        if (
+            isinstance(nearest_node, ast.SelectStmt | ast.UpdateStmt | ast.DeleteStmt)
+            and nearest_node.whereClause
+            or isinstance(nearest_node, ast.A_Expr)
+        ):
+            indent = 11
+            dedent = -4
+        elif bool_in_ancestors:
+            indent = 8
+            dedent = -4
+        else:
+            indent = 6
+            dedent = -4
+
         with output.push_indent(indent, relative=False):
             output.write("(")
             output.newline()
@@ -51,6 +66,21 @@ def sub_link(node: ast.SubLink, output: stream.RawStream) -> None:
             output.indent(dedent, relative=False)
             output.write(")")
             output.dedent()
+
+
+def subexpression_needs_parentheses(node: ast.SelectStmt) -> bool:
+    """Check if subexpression needs to be wrapped in parentheses in set operations.
+    A SELECT statement on either sides of UNION/INTERSECT/EXCEPT must be wrapped in
+    parentheses if it contains ORDER BY/LIMIT/... or is a nested UNION/INTERSECT/EXCEPT.
+    """
+    return bool(
+        node.sortClause
+        or node.limitCount
+        or node.limitOffset
+        or node.lockingClause
+        or node.withClause
+        or node.op != enums.SetOperation.SETOP_NONE,
+    )
 
 
 @printers.node_printer(ast.SelectStmt, override=True)
@@ -63,38 +93,44 @@ def select_stmt(node: ast.SelectStmt, output: stream.RawStream) -> None:
             output.print_node(node.withClause)
             output.indent()
 
-        so = enums.SetOperation
-
         if node.valuesLists:
-            # Is this a SELECT ... FROM (VALUES (...))?
-            with output.expression(isinstance(node.ancestors[0], ast.RangeSubselect)):
+            with output.expression(need_parens=False):
                 output.write("VALUES")
                 output.space()
                 output.print_lists(node.valuesLists, standalone_items=False)
-        elif node.op != so.SETOP_NONE and (node.larg or node.rarg):
+        elif node.op != enums.SetOperation.SETOP_NONE and (node.larg or node.rarg):
             with output.push_indent():
                 if node.larg:
                     with output.expression(
-                        printers.dml._select_needs_to_be_wrapped_in_parens(  # noqa: SLF001
+                        subexpression_needs_parentheses(
                             node.larg,
                         ),
                     ):
                         output.print_node(node.larg)
+
                 output.newline()
-                if node.op == so.SETOP_UNION:
+
+                if node.op == enums.SetOperation.SETOP_UNION:
                     output.space()
-                    output.write("UNION")
-                elif node.op == so.SETOP_INTERSECT:
+                    if not subexpression_needs_parentheses(node.larg):
+                        output.write("UNION")
+                    else:
+                        output.space()
+                        output.write("UNION")
+                elif node.op == enums.SetOperation.SETOP_INTERSECT:
                     output.write("INTERSECT")
-                elif node.op == so.SETOP_EXCEPT:
+                elif node.op == enums.SetOperation.SETOP_EXCEPT:
                     output.write("EXCEPT")
+
                 if node.all:
                     output.space()
                     output.write("ALL")
+
                 output.newline()
+
                 if node.rarg:
                     with output.expression(
-                        printers.dml._select_needs_to_be_wrapped_in_parens(  # noqa: SLF001
+                        subexpression_needs_parentheses(
                             node.rarg,
                         ),
                     ):
@@ -104,75 +140,93 @@ def select_stmt(node: ast.SelectStmt, output: stream.RawStream) -> None:
             if node.distinctClause:
                 output.space()
                 output.write("DISTINCT")
+
                 if node.distinctClause[0]:
                     output.space()
                     output.write("ON")
                     output.space()
                     with output.expression(need_parens=True):
                         output.print_list(node.distinctClause)
+
                 output.newline()
                 output.space(6)
+
             if node.targetList:
                 output.space()
                 output.print_list(node.targetList)
+
             if node.intoClause:
                 output.newline()
                 output.space(2)
                 output.write("INTO")
                 output.space()
+
                 if node.intoClause.rel.relpersistence == enums.RELPERSISTENCE_UNLOGGED:
                     output.write("UNLOGGED")
                     output.space()
                 elif node.intoClause.rel.relpersistence == enums.RELPERSISTENCE_TEMP:
                     output.write("TEMPORARY")
                     output.space()
+
                 output.print_node(node.intoClause)
+
             if node.fromClause:
                 output.newline()
                 output.space(2)
                 output.write("FROM")
                 output.space()
-                output.print_list(node.fromClause)
+                output.print_list(node.fromClause, standalone_items=True)
+
             if node.whereClause:
                 output.newline()
                 output.space()
                 output.write("WHERE")
                 output.space()
                 output.print_node(node.whereClause)
+
             if node.groupClause:
                 output.newline()
                 output.space()
                 output.write("GROUP BY")
                 output.space()
+
                 if node.groupDistinct:
                     output.write("DISTINCT")
                     output.space()
-                output.print_list(node.groupClause)
+
+                output.print_list(node.groupClause, standalone_items=True)
+
             if node.havingClause:
                 output.newline()
                 output.write("HAVING")
                 output.space()
                 output.print_node(node.havingClause)
+
             if node.windowClause:
                 output.newline()
                 output.write("WINDOW")
                 output.space()
                 output.print_list(node.windowClause)
+
         if node.sortClause:
             output.newline()
             output.space()
             output.write("ORDER BY")
             output.space()
             output.print_list(node.sortClause)
+
         if node.limitCount:
             output.newline()
+
             if node.limitOption == enums.LimitOption.LIMIT_OPTION_COUNT:
                 output.space()
                 output.write("LIMIT")
             elif node.limitOption == enums.LimitOption.LIMIT_OPTION_WITH_TIES:
                 output.space()
                 output.write("FETCH FIRST")
+
             output.space()
+
             if isinstance(node.limitCount, ast.A_Const) and node.limitCount.isnull:
                 output.write("ALL")
             else:
@@ -181,14 +235,17 @@ def select_stmt(node: ast.SelectStmt, output: stream.RawStream) -> None:
                     and node.limitCount.kind == enums.A_Expr_Kind.AEXPR_OP,
                 ):
                     output.print_node(node.limitCount)
+
             if node.limitOption == enums.LimitOption.LIMIT_OPTION_WITH_TIES:
                 output.space()
                 output.write("ROWS WITH TIES")
+
         if node.limitOffset:
             output.newline()
             output.write("OFFSET")
             output.space()
             output.print_node(node.limitOffset)
+
         if node.lockingClause:
             output.newline()
             output.write("FOR")
@@ -197,3 +254,25 @@ def select_stmt(node: ast.SelectStmt, output: stream.RawStream) -> None:
 
         if node.withClause:
             output.dedent()
+
+
+@printers.node_printer(ast.RangeSubselect, override=True)
+def range_subselect(node: ast.RangeSubselect, output: stream.RawStream) -> None:
+    """Printer for RangeSubselect."""
+    if node.lateral:
+        output.write("LATERAL")
+        output.space()
+
+    with output.push_indent():
+        output.write("(")
+        output.newline()
+        output.space(4)
+        output.print_node(node.subquery)
+        output.newline()
+        output.write(")")
+
+    if node.alias:
+        output.space()
+        output.write("AS")
+        output.space()
+        output.print_name(node.alias)
