d0a79426f4cd056ff5c4e091d8d6f2fb3acfc9ef
Bolaji Wahab <bolaji.wahab@n26.com>
2025-01-27 23:40:09 +0100

    chore(ci): add test for GN035

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -5,7 +5,7 @@ from pgrubic import core
 
 def test_load_rules(linter: core.Linter) -> None:
     """Test loading rules."""
-    expected_number_of_rules = 107
+    expected_number_of_rules = 108
 
     rules = core.load_rules(config=linter.config)
 
