ed09455b80f1b5d0b2e25d1eacf5015a7f4b8126
Bolaji Wahab <bolajiwahab23@gmail.com>
2025-05-22 01:26:59 +0200

    fix: cleanup unused error codes

--- src/pgrubic/core/errors.py
+++ src/pgrubic/core/errors.py
@@ -8,22 +8,6 @@ from colorama import Fore, Style
 from pgrubic.core import noqa
 
 
-class BaseError(Exception):
-    """Base class for all exceptions."""
-
-
-class MissingConfigError(BaseError):
-    """Raised when a config is missing."""
-
-
-class ParseError(BaseError):
-    """Raised when a parse error occurs."""
-
-
-class MissingStatementTerminatorError(BaseError):
-    """Raised when a statement terminator is missing."""
-
-
 class Error(typing.NamedTuple):
     """Representation of an error."""
 
