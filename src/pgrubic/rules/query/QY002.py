70751cd032e3bec596c91f7ab4d6a43bfdb6b4ba
bolajiwahab <bolajiwahab23@gmail.com>
2025-08-18 15:45:04 +0200

    feat(rule): use inplace conditionals

--- src/pgrubic/rules/query/QY002.py
+++ src/pgrubic/rules/query/QY002.py
@@ -20,14 +20,6 @@ class OrdinalNumberOrderBy(linter.BaseChecker):
     Explicit column names or expressions.
     """
 
-    @staticmethod
-    def _is_ordinal_sort_column(column: ast.SortBy) -> bool:
-        """Check for ordinal sort column."""
-        return isinstance(column.node, ast.A_Const) and isinstance(
-            column.node.val,
-            ast.Integer,
-        )
-
     def visit_SelectStmt(
         self,
         ancestors: visitors.Ancestor,
@@ -35,7 +27,9 @@ class OrdinalNumberOrderBy(linter.BaseChecker):
     ) -> None:
         """Visit SelectStmt."""
         if node.sortClause and any(
-            isinstance(column, ast.SortBy) and self._is_ordinal_sort_column(column)
+            isinstance(column, ast.SortBy)
+            and isinstance(column.node, ast.A_Const)
+            and isinstance(column.node.val, ast.Integer)
             for column in node.sortClause
         ):
             self.violations.add(
